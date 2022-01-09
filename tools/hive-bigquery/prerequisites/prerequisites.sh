f5d799d0824e5a0d0039332e8ecc02f76ac78a34
Vince Gonzalez <vicenteg@users.noreply.github.com>
2022-01-09 15:49:00 -0500

    Update prerequisites.sh
    Since it's unlikely it's necessary to update all packages on the system as a prerequisite only on yum-based systems, remove `yum -y update`.
    
    #747

--- tools/hive-bigquery/prerequisites/prerequisites.sh
+++ tools/hive-bigquery/prerequisites/prerequisites.sh
@@ -26,7 +26,6 @@ if apt-get -h ; then
 else
     if yum -h ; then
         echo "Package manager is yum"
-        yum -y update
         yum install -y wget
         yum install -y mysql
         yum install -y gcc gcc-c++ cyrus-sasl-devel
